ea5b2276afbf49128aee0a5af06304e84c9c1b29
Gaurav Gosain <gg68@hw.ac.uk>
2022-09-22 22:32:57 +0400

    fixed broken next image

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/future/image";
 import styles from "../styles/Home.module.css";
 import { SiInstagram, SiYoutube, SiTwitter, SiLinkedin } from "react-icons/si";
 
@@ -13,11 +12,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Image
+        <img
           src="/gdsclogo.png"
           alt="Google Developer Student Clubs Logo"
           width={400}
           height={400}
+          className="w-[80vw] max-w-[400px]"
         />
 
         <p className={"text-4xl my-10"}>Heriot-Watt University</p>
@@ -25,7 +25,9 @@ export default function Home() {
         <div className={styles.grid}>
           <a
             href="https://gdsc.community.dev/heriot-watt-university/"
-            without rel="noreferrer" target={"_blank"}
+            without
+            rel="noreferrer"
+            target={"_blank"}
             className={"card hover:border-red-400 hover:text-red-500"}>
             <h2>GDSC Community Page</h2>
           </a>
@@ -36,25 +38,33 @@ export default function Home() {
             <div className="flex w-full justify-evenly">
               <a
                 href="https://instagram.com/gdsc_hwu?igshid=YmMyMTA2M2Y="
-                without rel="noreferrer" target={"_blank"}
+                without
+                rel="noreferrer"
+                target={"_blank"}
                 className="hover:text-pink-500 hover:scale-110 hover:duration-150 p-4 rounded-full bg-[#fcfdfa88]">
                 <SiInstagram fontSize={"35px"} />
               </a>
               <a
                 href="https://www.youtube.com/channel/UC8jF4bov22bDnf9pClPigvg"
-                without rel="noreferrer" target={"_blank"}
+                without
+                rel="noreferrer"
+                target={"_blank"}
                 className="hover:text-red-500 hover:scale-110 hover:duration-150 p-4 rounded-full bg-[#fcfdfa88]">
                 <SiYoutube fontSize={"35px"} />
               </a>
               <a
                 href="https://twitter.com/gdsc_hwu"
-                without rel="noreferrer" target={"_blank"}
+                without
+                rel="noreferrer"
+                target={"_blank"}
                 className="hover:text-blue-800 hover:scale-110 hover:duration-150 p-4 rounded-full bg-[#fcfdfa88]">
                 <SiTwitter fontSize={"35px"} />
               </a>
               <a
                 href="https://www.linkedin.com/company/gdsc-hwu"
-                without rel="noreferrer" target={"_blank"}
+                without
+                rel="noreferrer"
+                target={"_blank"}
                 className="hover:text-purple-500 hover:scale-110 hover:duration-150 p-4 rounded-full bg-[#fcfdfa88]">
                 <SiLinkedin fontSize={"35px"} />
               </a>
@@ -63,31 +73,23 @@ export default function Home() {
 
           <a
             href="https://forms.office.com/Pages/ResponsePage.aspx?id=DQSIkWdsW0yxEjajBLZtrQAAAAAAAAAAAANAAQVOwVpUMkhKQjFUR1NCWFVPUlhSUDRQNThKMUNEVC4u"
-            without rel="noreferrer" target={"_blank"}
+            without
+            rel="noreferrer"
+            target={"_blank"}
             className={"card hover:border-orange-400 hover:text-orange-500"}>
             <h2>GDSC @ HWU - Sign-Up Form</h2>
           </a>
 
           <a
             href="https://youtube.com/playlist?list=PLYlB334TcbXGVBqG-7m_udB-QTWQXYCf8"
-            without rel="noreferrer" target={"_blank"}
+            without
+            rel="noreferrer"
+            target={"_blank"}
             className={"card hover:border-blue-400 hover:text-blue-500"}>
             <h2>Creating your own Web App Series - Playlist</h2>
           </a>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer">
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 }
