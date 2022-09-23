e6623992404b6689c5e123f45e780e79450f643e
Gaurav Gosain <gg68@hw.ac.uk>
2022-09-23 15:39:29 +0400

    removing redundant code

--- pages/_app.js
+++ pages/_app.js
@@ -4,11 +4,6 @@ import "../styles/globals.css";
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta name="description" content="GDSC Link Tree" />
-        <title>GDSC Link Tree</title>
-        <link rel="icon" href="/gdsc-logo.png" />
-      </Head>
       <Component {...pageProps} />
     </>
   );
