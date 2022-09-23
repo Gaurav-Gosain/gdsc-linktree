63a7fd7f54021203e4a7765740798342ccbfd08b
Gaurav Gosain <gg68@hw.ac.uk>
2022-09-23 12:27:36 +0400

    reduced css and modularized code

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="description" content="GDSC Link Tree" />
+        <title>GDSC Link Tree</title>
+        <link rel="icon" href="/gdsc-logo.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
